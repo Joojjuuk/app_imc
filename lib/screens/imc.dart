5ad462d1f44d8e45b8010bc8bc9473f32b89894e
Joojjuuk <joojjuuk@gmail.com>
2025-04-08 10:26:20 -0300

    Tela Inicial - Base

---
+++ lib/screens/imc.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class IMC extends StatelessWidget {
+  IMC({super.key});
+
+  final TextEditingController alturaController = TextEditingController();
+  final TextEditingController pesoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
+            child: Text(
+              "Bem vindo ao APP Calculo de IMC!!!",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xFF664FA3),
+                  fontWeight: FontWeight.w800),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40),
+            child: TextField(
+              controller: alturaController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
+              ],
+              decoration: InputDecoration(
+                  labelText: "Altura em Metros",
+                  labelStyle:
+                      TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+                  hintText: "1.78",
+                  hintStyle: TextStyle(
+                    color: Color(0xCC000000),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 17,
+                  ),
+                  suffixText: 'm',
+                  suffixStyle: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                    color: Color(0xCC000000),
+                  )),
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xB3000000)),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
+              child: TextField(
+                controller: pesoController,
+                keyboardType:
+                    TextInputType.numberWithOptions(decimal: true),
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
+                ],
+                decoration: InputDecoration(
+                    labelText: "Peso em KGs",
+                    labelStyle: TextStyle(
+                        fontWeight: FontWeight.w700, fontSize: 20),
+                    hintText: "60",
+                    hintStyle: TextStyle(
+                      color: Color(0xCC000000),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 17,
+                    ),
+                    suffixText: "KG",
+                    suffixStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      color: Color(0xCC000000),
+                    )),
+              )
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(40, 20, 40, 10),
+            child:ElevatedButton(
+              onPressed: () {
+                final altura = double.tryParse(alturaController.text);
+                final peso = double.tryParse(pesoController.text); },
+              child: Text("Calcular IMC"),
+            )
+            ),
+        ]),
+      ),
+    ));
+  }
+}
