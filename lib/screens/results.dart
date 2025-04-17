cd6f3577cc375b5a077417b153382e847d9611a3
Joojjuuk <joojjuuk@gmail.com>
2025-04-17 01:20:09 -0300

    Tela Resultados - v1 - Base + Mudanca de estilo

--- lib/screens/results.dart
+++ lib/screens/results.dart
@@ -1,3 +1,4 @@
+import 'package:app_imc/logic/imc_calculation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -8,23 +9,101 @@ class Results extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String classificacao = classificarIMC(imc);
+    String Descricaoclas;
+    if (classificacao == "Magreza grave") {
+      Descricaoclas = "Você está com magreza grave. Procure um médico.";
+    } else if (classificacao == "Magreza moderada") {
+      Descricaoclas = "Você está com magreza moderada. Procure um médico.";
+    } else if (classificacao == "Magreza leve") {
+      Descricaoclas = "Você está com magreza leve. Procure um médico.";
+    } else if (classificacao == "Saudável") {
+      Descricaoclas = "Parabéns! Seu peso está saudável.";
+    } else if (classificacao == "Sobrepeso") {
+      Descricaoclas = "Você está com sobrepeso. Procure um médico.";
+    } else if (classificacao == "Obesidade Grau 1") {
+      Descricaoclas = "Você está com obesidade grau 1. Procure um médico.";
+    } else if (classificacao == "Obesidade Grau 2") {
+      Descricaoclas = "Você está com obesidade grau 2. Procure um médico.";
+    } else {
+      Descricaoclas = "Você está com obesidade grau 3. Procure um médico.";
+    }
     return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/logo/BMI-Logo.png',
+              fit: BoxFit.contain,
+              height: 100,
+            ),
+            Text(
+              'Calculator',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25,
+                color: Color(0xffffffff),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(
-        child: Center(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
             children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
-                child: Text(
-                  "Resultado do IMC: ${imc.toStringAsFixed(2)}",
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Color(0xFF664FA3),
-                      fontWeight: FontWeight.w800),
+              Text('Aqui está seu resultado!!',
                   textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                  )),
+              Image.asset(
+                'assets/cartoons/Obesidade.png',
+                scale: 2,
+              ),
+              Text('Seu imc é: $imc',
+                  style: TextStyle(
+                    color: Color(0xFFFFCE00),
+                    fontSize: 38,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                Descricaoclas,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.normal,
                 ),
-              )
+              ),
+              SizedBox(height: 25,),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFFFCE00),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: EdgeInsets.fromLTRB(70, 7, 70, 7)),
+                  onPressed: () {},
+                  child: Text(
+                    'Salvar Resultado',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.black,
+                    ),
+                  )),
             ],
           ),
         ),
